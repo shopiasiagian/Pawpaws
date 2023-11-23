c8098c165267267bd3dd9a44d7f3e0b933f5e712
shopiasiagian <122168889+shopiasiagian@users.noreply.github.com>
2023-11-23 10:07:08 +0700

    Add files via upload

---
+++ config/database.php
@@ -0,0 +1,7 @@
+<?php
+    $config = [
+        'server' => '127.0.0.1', #IP default local host.
+        'username' => 'root', #Default
+        'password' => '', #Default
+        'database' => 'petshop', #Sesuai sama nama database yang udah dibuat.
+    ];
